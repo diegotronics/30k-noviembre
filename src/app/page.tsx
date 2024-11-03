9690388cfe2e9209c891a3ca8a8ad9c3588a6a7e
Diego Perez <diegoperez1295@gmail.com>
2024-11-03 11:22:34 -0400

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Countdown from 'react-countdown'
-import { Sparkles, TrophyIcon, ClockIcon } from 'lucide-react'
+import { Sparkles, TrophyIcon, ClockIcon, GroupIcon } from 'lucide-react'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
 type Player = {
@@ -84,31 +84,31 @@ export default function Component() {
     const restOfPlayers = players.slice(3)
 
     return (
-        <div className="min-h-screen bg-gradient-to-b from-orange-600 to-orange-700 p-6">
+        <div className="min-h-screen bg-gradient-to-b from-orange-500 to-orange-700 p-6">
             <div className="max-w-md mx-auto">
-                <h1 className="text-4xl font-bold text-white text-center mb-8">
+                <h1 className="text-3xl sm:text-4xl font-bold text-white text-center mb-8">
                     Reto 30k Noviembre
                 </h1>
 
-                <div className="bg-white shadow overflow-hidden rounded-full mb-8 p-4 px-8 grid grid-cols-2">
-                    <div className="">
-                        <span className="text-lg leading-6 font-medium text-gray-900 flex items-center">
-                            <TrophyIcon className="h-6 w-6 text-yellow-600 mr-2" />
+                <div className="bg-white shadow overflow-hidden rounded mb-14 p-2 grid grid-cols-2">
+                    <div className="flex flex-col text-center">
+                        <p className="text-md leading-6 font-medium text-gray-900 flex items-center">
+                            <TrophyIcon className="h-5 w-5 text-yellow-600 mr-2" />
                             Pote
-                        </span>
-                        <p className="mt-2 pl-8 text-2xl font-semibold text-gray-900">
+                        </p>
+                        <p className="mt-2 text-2xl font-semibold text-gray-900">
                             $10
                         </p>
                     </div>
-                    <div className="">
-                        <h2 className="text-lg leading-6 font-medium text-gray-900 flex items-center">
-                            <ClockIcon className="h-6 w-6 text-blue-500 mr-2" />
+                    <div className="flex flex-col text-center">
+                        <h2 className="text-md leading-6 font-medium text-gray-900 flex items-center">
+                            <ClockIcon className="h-5 w-5 text-blue-500 mr-2" />
                             Tiempo Restante
                         </h2>
                         <Countdown
                             date={endDate}
                             renderer={({ days, hours, minutes, seconds }) => (
-                                <div className="mt-2 pl-8 text-2xl font-semibold text-gray-900">
+                                <div className="mt-2 pl-2 text-2xl font-semibold text-gray-900">
                                     {String(days).padStart(2, '0')}:
                                     {String(hours).padStart(2, '0')}:
                                     {String(minutes).padStart(2, '0')}:
@@ -195,6 +195,9 @@ export default function Component() {
 
                 {/* Rest of players list */}
                 <div className="bg-white rounded-xl p-4">
+                    <div className="text-sm text-gray-500 flex items-center">
+                        Atletas Inscritos: {players.length}
+                    </div>
                     <ScrollArea className="h-64 w-full pr-4">
                         {restOfPlayers.map((player, index) => (
                             <motion.div
@@ -205,15 +208,15 @@ export default function Component() {
                                 transition={{ delay: index * 0.1 + 0.5 }}
                             >
                                 <div className="flex items-center gap-3">
-                                    <span className="flex items-center justify-center h-12 w-12 rounded-full bg-gray-200 mr-4 text-2xl font-bold rest">
+                                    <span className="flex items-center justify-center h-12 w-12 rounded-full bg-gray-200 mr-4 text-2xl font-bold shrink-0">
                                         {player.position}
                                     </span>
                                     <span className="font-medium">
                                         {player.name}
                                     </span>
                                 </div>
-                                <div className="flex items-center gap-4 pr-4">
-                                    <span className="font-bold text-2xl">
+                                <div className="flex items-center gap-4 pr-2 shrink-0">
+                                    <span className="font-bold text-xl">
                                         {player.km} km
                                     </span>
                                 </div>
